94037f06be5b41f19848ff3c6310e8136d60f66e
HannesSommer <hannes.sommer@mavt.ethz.ch>
2013-11-07 11:44:10 +0100

    made aslam_splines/__init__.py load the aslam_backend on which it depends

--- aslam_splines_python/python/aslam_splines/__init__.py
+++ aslam_splines_python/python/aslam_splines/__init__.py
@@ -1,6 +1,9 @@
 # Import the numpy to Eigen type conversion.
-import roslib; roslib.load_manifest('numpy_eigen'); import numpy_eigen
-# Import the the C++ exports from your package library.
+import roslib; 
+roslib.load_manifest('numpy_eigen'); import numpy_eigen
+roslib.load_manifest('aslam_backend_python'); import aslam_backend
+
+ # Import the the C++ exports from your package library.
 from libaslam_splines_python import *
 # Import other files in the directory
 # from mypyfile import *
